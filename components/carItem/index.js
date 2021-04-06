a9ef49230036970918c76db30f43bd08989f22fe
Dan Akmat <akmatdan@gmail.com>
2021-04-07 00:35:25 +0600

    Car Item component

--- components/carItem/index.js
+++ components/carItem/index.js
@@ -3,20 +3,30 @@ import { View, Text, ImageBackground } from 'react-native'
 import StyledButton from '../StyledButton'
 import styles from './styles'
 
-const CarItem = () => {
+const CarItem = (props) => {
+
+  const { name, tagline, taglineCTA, image } = props
+
     return (
         <View style={styles.carContainer}>
         
         <ImageBackground 
-          source={require('../../assets/assets-2/images/ModelS.jpeg')}
+          source={image}
           style={styles.image}
         />
 
         <View style={styles.titles}>
-          <Text style={styles.title}>Model S</Text>
-          <Text style={styles.subtitle}>Starting at $69,420</Text>
+          <Text style={styles.title}>{name}</Text>
+          <Text style={styles.subtitle}>
+            {tagline}
+            {' '}
+            <Text style={styles.subtitleCTA}>
+              {taglineCTA}
+            </Text>
+          </Text>
         </View>
 
+        <View style={styles.buttonsContainer}>
         <StyledButton 
           type="primary"
           content={"Customer Order"}
@@ -32,6 +42,7 @@ const CarItem = () => {
             console.warn("Existing Inventory was pressed")
           }}
         />
+        </View>
 
       </View>
     )
